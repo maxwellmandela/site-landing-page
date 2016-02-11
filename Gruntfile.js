5e02b60bc3964fd30ee7e6ba783b1531ecc68d4d
sigu <sigumagwa@gmail.com>
2016-02-11 23:50:38 +0300

    only zooom animations

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,165 +1,164 @@
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+	require('load-grunt-tasks')(grunt);
 
-    require('load-grunt-tasks')(grunt);
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		app: 'app',
+		dist: 'dist',
 
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded', // expanded or nested or compact or compressed
+					loadPath: '<%= app %>/bower_components/foundation/scss',
+					compass: true,
+					quiet: true
+				},
+				files: {
+					'<%= app %>/css/app.css': '<%= app %>/scss/app.scss'
+				}
+			}
+		},
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        app: 'app',
-        dist: 'dist',
+		postcss: {
+			options: {
+				processors: [
+					require('autoprefixer')({
+						browsers: 'last 2 versions'
+					})
+				]
+			},
+			dist: {
+				src: '<%= app %>/css/app.css'
+			}
+		},
 
-        sass: {
-            dist: {
-                options: {
-                    style: 'expanded', // expanded or nested or compact or compressed
-                    loadPath: '<%= app %>/bower_components/foundation/scss',
-                    compass: true,
-                    quiet: true
-                },
-                files: {
-                    '<%= app %>/css/app.css': '<%= app %>/scss/app.scss'
-                }
-            }
-        },
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'<%= app %>/js/**/*.js'
+			]
+		},
 
-        postcss: {
-            options: {
-                processors: [
-                    require('autoprefixer')({browsers: 'last 2 versions'})
-                ]
-            },
-            dist: {
-                src: '<%= app %>/css/app.css'
-            }
-        },
+		clean: {
+			dist: {
+				src: ['<%= dist %>/*']
+			}
+		},
+		copy: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '<%= app %>/',
+					src: ['fonts/**', '**/*.html', '!**/*.scss', '!bower_components/**'],
+					dest: '<%= dist %>/'
+				}, {
+					expand: true,
+					flatten: true,
+					src: ['<%= app %>/bower_components/font-awesome/fonts/**'],
+					dest: '<%= dist %>/fonts/',
+					filter: 'isFile'
+				}]
+			}
+		},
 
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc'
-            },
-            all: [
-                'Gruntfile.js',
-                '<%= app %>/js/**/*.js'
-            ]
-        },
+		imagemin: {
+			target: {
+				files: [{
+					expand: true,
+					cwd: '<%= app %>/images/',
+					src: ['**/*.{jpg,gif,svg,jpeg,png}'],
+					dest: '<%= dist %>/images/'
+				}]
+			}
+		},
 
-        clean: {
-            dist: {
-                src: ['<%= dist %>/*']
-            },
-        },
-        copy: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd:'<%= app %>/',
-                    src: ['fonts/**', '**/*.html', '!**/*.scss', '!bower_components/**'],
-                    dest: '<%= dist %>/'
-                } , {
-                    expand: true,
-                    flatten: true,
-                    src: ['<%= app %>/bower_components/font-awesome/fonts/**'],
-                    dest: '<%= dist %>/fonts/',
-                    filter: 'isFile'
-                } ]
-            },
-        },
+		uglify: {
+			options: {
+				preserveComments: 'some',
+				mangle: false
+			}
+		},
 
-        imagemin: {
-            target: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= app %>/images/',
-                    src: ['**/*.{jpg,gif,svg,jpeg,png}'],
-                    dest: '<%= dist %>/images/'
-                }]
-            }
-        },
+		useminPrepare: {
+			html: ['<%= app %>/index.html'],
+			options: {
+				dest: '<%= dist %>'
+			}
+		},
 
-        uglify: {
-            options: {
-                preserveComments: 'some',
-                mangle: false
-            }
-        },
+		usemin: {
+			html: ['<%= dist %>/**/*.html', '!<%= app %>/bower_components/**'],
+			css: ['<%= dist %>/css/**/*.css'],
+			options: {
+				dirs: ['<%= dist %>']
+			}
+		},
 
-        useminPrepare: {
-            html: ['<%= app %>/index.html'],
-            options: {
-                dest: '<%= dist %>'
-            }
-        },
+		watch: {
+			grunt: {
+				files: ['Gruntfile.js'],
+				tasks: ['sass', 'postcss']
+			},
+			sass: {
+				files: '<%= app %>/scss/**/*.scss',
+				tasks: ['sass', 'postcss']
+			},
+			livereload: {
+				files: ['<%= app %>/**/*.html', '!<%= app %>/bower_components/**', '<%= app %>/js/**/*.js', '<%= app %>/css/**/*.css', '<%= app %>/images/**/*.{jpg,gif,svg,jpeg,png}'],
+				options: {
+					livereload: true
+				}
+			}
+		},
 
-        usemin: {
-            html: ['<%= dist %>/**/*.html', '!<%= app %>/bower_components/**'],
-            css: ['<%= dist %>/css/**/*.css'],
-            options: {
-                dirs: ['<%= dist %>']
-            }
-        },
+		connect: {
+			app: {
+				options: {
+					port: 9000,
+					base: '<%= app %>/',
+					open: true,
+					livereload: true,
+					hostname: '127.0.0.1'
+				}
+			},
+			dist: {
+				options: {
+					port: 9001,
+					base: '<%= dist %>/',
+					open: true,
+					keepalive: true,
+					livereload: false,
+					hostname: '127.0.0.1'
+				}
+			}
+		},
 
-        watch: {
-            grunt: {
-                files: ['Gruntfile.js'],
-                tasks: ['sass', 'postcss']
-            },
-            sass: {
-                files: '<%= app %>/scss/**/*.scss',
-                tasks: ['sass', 'postcss']
-            },
-            livereload: {
-                files: ['<%= app %>/**/*.html', '!<%= app %>/bower_components/**', '<%= app %>/js/**/*.js', '<%= app %>/css/**/*.css', '<%= app %>/images/**/*.{jpg,gif,svg,jpeg,png}'],
-                options: {
-                    livereload: true
-                }
-            }
-        },
+		wiredep: {
+			target: {
+				src: [
+					'<%= app %>/**/*.html'
+				],
+				exclude: [
+					'modernizr',
+					'font-awesome',
+					'jquery-placeholder',
+					'foundation'
+				]
+			}
+		}
 
-        connect: {
-            app: {
-                options: {
-                    port: 9000,
-                    base: '<%= app %>/',
-                    open: true,
-                    livereload: true,
-                    hostname: '127.0.0.1'
-                }
-            },
-            dist: {
-                options: {
-                    port: 9001,
-                    base: '<%= dist %>/',
-                    open: true,
-                    keepalive: true,
-                    livereload: false,
-                    hostname: '127.0.0.1'
-                }
-            }
-        },
+	});
 
-        wiredep: {
-            target: {
-                src: [
-                    '<%= app %>/**/*.html'
-                ],
-                exclude: [
-                    'modernizr',
-                    'font-awesome',
-                    'jquery-placeholder',
-                    'foundation'
-                ]
-            }
-        }
-
-    });
-
-    grunt.registerTask('compile-sass', ['sass', 'postcss']);
-    grunt.registerTask('bower-install', ['wiredep']);
-    grunt.registerTask('default', ['compile-sass', 'bower-install', 'connect:app', 'watch']);
-    grunt.registerTask('validate-js', ['jshint']);
-    grunt.registerTask('server-dist', ['connect:dist']);
-    grunt.registerTask('publish', ['compile-sass', 'clean:dist', 'validate-js', 'useminPrepare', 'copy:dist', 'newer:imagemin', 'concat', 'cssmin', 'uglify', 'usemin']);
-
+	grunt.registerTask('compile-sass', ['sass', 'postcss']);
+	grunt.registerTask('bower-install', ['wiredep']);
+	grunt.registerTask('default', ['compile-sass', 'bower-install', 'connect:app', 'watch']);
+	grunt.registerTask('validate-js', ['jshint']);
+	grunt.registerTask('server-dist', ['connect:dist']);
+	grunt.registerTask('publish', ['compile-sass', 'clean:dist', 'validate-js', 'useminPrepare', 'copy:dist', 'newer:imagemin', 'concat', 'cssmin', 'uglify', 'usemin']);
 };
--- app/js/app.js
+++ app/js/app.js
@@ -1,45 +1,51 @@
-/*jslint browser: true*/
-/*global $, jQuery, WOW*/
+/* jslint browser: true*/
+/* global $, jQuery, WOW, location*/
 'use strict';
 
-var app = (function(document, $) {
-	var docElem = document.documentElement,
-		_userAgentInit = function() {
-			docElem.setAttribute('data-useragent', navigator.userAgent);
-		},
-		_init = function() {
-			$(document).foundation();
-            // needed to use joyride
-            // doc: http://foundation.zurb.com/docs/components/joyride.html
-            $(document).on('click', '#start-jr', function () {
-                $(document).foundation('joyride', 'start');
-            });
-			_userAgentInit();
-		};
+var app = (function (document, $) {
+	var docElem = document.documentElement;
+	var _userAgentInit = function () {
+		docElem.setAttribute('data-useragent', navigator.userAgent);
+	};
+	var _init = function () {
+		$(document)
+			.foundation();
+		// needed to use joyride
+		// doc: http://foundation.zurb.com/docs/components/joyride.html
+		$(document)
+			.on('click', '#start-jr', function () {
+				$(document)
+					.foundation('joyride', 'start');
+			});
+		_userAgentInit();
+	};
 	return {
 		init: _init
 	};
 })(document, jQuery);
 
-(function() {
+(function () {
 	app.init();
 })();
 
-
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1500);
-        return false;
-      }
-    }
-  });
+$(function () {
+	$('a[href*=#]:not([href=#])')
+		.click(function () {
+			if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html,body')
+						.animate({
+							scrollTop: target.offset()
+								.top
+						}, 1500);
+					return false;
+				}
+			}
+		});
 });
 
-new WOW().init();
+new WOW()
+	.init();
 // var wow = new WOW();
